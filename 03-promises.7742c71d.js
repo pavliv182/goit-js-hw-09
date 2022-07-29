6a8f411c91f49d0c90c5d32f4514ba20b3e99ed2
pavliv182 <97523241+pavliv182@users.noreply.github.com>
2022-07-29 14:51:23 +0000

    Deploying to gh-pages from @ pavliv182/goit-js-hw-09@73d89d1dfecf8498aaa96db7852fb8fd143cbd7f 🚀

--- 03-promises.7742c71d.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=new Promise((function(o,e){setTimeout((function(){o("Success! Value passed to resolve function")}),2e3)}));console.log("Before promise.then()"),o.then((function(o){console.log("onResolve call inside promise.then()"),console.log(o)}),(function(o){console.log("onReject call inside promise.then()"),console.log(o)})),console.log("After promise.then()")}();
-//# sourceMappingURL=03-promises.7742c71d.js.map
