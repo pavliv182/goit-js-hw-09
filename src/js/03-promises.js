73d89d1dfecf8498aaa96db7852fb8fd143cbd7f
pavliv182 <uptn182@gmail.com>
2022-07-29 17:50:51 +0300

    task 03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,41 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+// delayEl: document.querySelector('input[name="delay"]'),
+//   stepEl: document.querySelector('input[name="step"]'),
+//   amountEl: document.querySelector('input[name="amount"]'),
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  submitBtnEl: document.querySelector('button[type="submit"]'),
+};
+refs.formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const formElements = evt.currentTarget.elements;
+  // console.log(formElements);
+  let delay = Number(formElements.delay.value);
+  let step = Number(formElements.step.value);
+  let amount = Number(formElements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
-}
-
-// Change value of isSuccess variable to call resolve or reject
-const isSuccess = true;
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve('Success! Value passed to resolve function');
-    } else {
-      reject('Error! Error passed to reject function');
-    }
-  }, 2000);
 });
 
-// Will run first
-console.log('Before promise.then()');
-
-// Registering promise callbacks
-promise.then(
-  // onResolve will run third or not at all
-  value => {
-    console.log('onResolve call inside promise.then()');
-    console.log(value); // "Success! Value passed to resolve function"
-  },
-  // onReject will run third or not at all
-  error => {
-    console.log('onReject call inside promise.then()');
-    console.log(error); // "Error! Error passed to reject function"
-  }
-);
-
-// Will run second
-console.log('After promise.then()');
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
